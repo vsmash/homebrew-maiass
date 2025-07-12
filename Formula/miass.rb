227ae55f002af3c4bc6ef1c9e1f9e93b1551aaf4
Mark Pottie <vsmash@users.noreply.github.com>
2025-07-12 23:18:28 +1000

    updated to v4.5.1

--- Formula/miass.rb
+++ Formula/miass.rb
@@ -1,10 +1,14 @@
 class Myass < Formula
     desc "Alias for MAIASS"
     homepage "https://github.com/vsmash/maiass"
-    url "https://github.com/vsmash/maiass/archive/refs/tags/v4.5.0.tar.gz"
+    url "https://github.com/vsmash/maiass/archive/refs/tags/4.5.1.tar.gz"
     sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
     license "GPL-3.0-only"
-  
+    version "4.5.1"
+    
+    depends_on "bash"
+    depends_on "jq"
+
     def install
       bin.install "maiass.sh" => "maiass"
       bin.install_symlink "maiass" => "myass"
