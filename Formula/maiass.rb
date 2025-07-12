be3fe5096462922bcef9542b156fae1c08555553
Mark Pottie <vsmash@users.noreply.github.com>
2025-07-12 22:52:46 +1000

    fixed license info

--- Formula/maiass.rb
+++ Formula/maiass.rb
@@ -3,7 +3,7 @@ class Maiass < Formula
     homepage "https://github.com/vsmash/maiass"
     url "https://github.com/vsmash/maiass/archive/refs/tags/v4.5.0.tar.gz"
     sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
-    license "MIT"
+    license "GPL-3.0-only"
     version "4.5.0"
   
     depends_on "bash"
