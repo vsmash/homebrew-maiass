ee787e20d6f9cf600a37ce7bc3f206aa8128fbcc
Mark Pottie <vsmash@users.noreply.github.com>
2025-07-12 23:30:15 +1000

    fixed aliases

--- Formula/myass.rb
+++ Formula/myass.rb
@@ -5,14 +5,19 @@ class Myass < Formula
     sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
     license "GPL-3.0-only"
     version "4.5.1"
-    
+  
     depends_on "bash"
     depends_on "jq"
   
     def install
-      bin.install "maiass.sh" => "maiass"
-      bin.install_symlink "maiass" => "myass"
-      bin.install_symlink "maiass" => "miass"
+        bin.install "maiass.sh" => "maiass"
+        bin.install_symlink "maiass" => "myass"
+        bin.install_symlink "maiass" => "miass"
+    end
+  
+    test do
+      assert_match "MAIASS", shell_output("#{bin}/maiass --help")
     end
   end
+  
   
