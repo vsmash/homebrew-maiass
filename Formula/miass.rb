c838450cb53682a3d12095b0e806871757d395eb
vsmsh <vsmash@users.noreply.github.com>
2025-07-14 14:34:55 +1000

    Added AI commit script to installation - feat: installed `aicommit.sh` alongside main scripts in all formulae - style: removed trailing whitespace across all files - fix: ensured file ends with a newline

--- Formula/miass.rb
+++ Formula/miass.rb
@@ -5,37 +5,38 @@ class Miass < Formula
     sha256 "9a44bc35c9758334c9d13618fe9637f0668c50e9ec8692f6e7dc65782ec568d6"
     license "GPL-3.0-only"
     version "4.8.3"
-  
+
     depends_on "bash"
     depends_on "jq"
-  
+
     def install
       bin.install "maiass.sh" => "maiass"
+      bin.install "aicommit.sh" => "aicommit"
       bin.install "package.json"
       bin.install_symlink "maiass" => "myass"
       bin.install_symlink "maiass" => "miass"
     end
-  
+
     test do
       assert_match "MAIASS", shell_output("#{bin}/maiass --help")
     end
   end
-  
+
   def caveats
     <<~EOS
       🧠 MAIASS has been installed!
-  
+
       You can now use:
         maiass       # Main command
         myass, miass # Shortcut aliases
-  
+
       To view usage:
         maiass --help
-  
+
       To enable AI commit messages:
         export MAIASS_OPENAI_TOKEN=your_api_key
         export MAIASS_OPENAI_MODE=ask
-  
+
       Full docs: https://github.com/vsmash/maiass
     EOS
   end
