14fbe36792d5538737e49eaa95c04c9553e185ff
vsmsh <vsmash@users.noreply.github.com>
2025-07-15 14:49:54 +1000

    Update to version 4.8.31

--- Formula/committhis.rb
+++ Formula/committhis.rb
@@ -1,7 +1,7 @@
-class Aicommit < Formula
+class committhis < Formula
     desc "AI powered Git commit messages"
-    homepage "https://github.com/vsmash/aicommit"
-    url "https://github.com/vsmash/aicommit/archive/refs/tags/4.8.28.tar.gz"
+    homepage "https://github.com/vsmash/committhis"
+    url "https://github.com/vsmash/committhis/archive/refs/tags/4.8.28.tar.gz"
     sha256 "0ac8ba9409777338e3618975a049df29280be7ed17196c8df4376589921e417c"
     license "GPL-3.0-only"
     version "4.8.28"
@@ -11,8 +11,9 @@ class Aicommit < Formula
 
     def install
         bin.install "maiass.sh" => "maiass"
-        bin.install "aicommit.sh" => "aicommit"
+        bin.install "committhis.sh" => "committhis"
         bin.install "package.json"
+        bin.install_symlink "committhis" => "aic"
         bin.install_symlink "maiass" => "myass"
         bin.install_symlink "maiass" => "miass"
     end
--- Formula/maiass.rb
+++ Formula/maiass.rb
@@ -1,17 +1,17 @@
 class Maiass < Formula
     desc "Modular AI-Assisted Semantic Savant for Git workflows"
     homepage "https://github.com/vsmash/maiass"
-    url "https://github.com/vsmash/maiass/archive/refs/tags/4.8.28.tar.gz"
-    sha256 "a7d428f3238feae3f495e8d1d0f70b7a3ff21181cbc480e5f2e6134e7cfeca98"
+    url "https://github.com/vsmash/maiass/archive/refs/tags/4.8.31.tar.gz"
+    sha256 "0d10ad9c010f5d07663fabc00ec2b6d8200b1226ac00f8fc9cb1065503037800"
     license "GPL-3.0-only"
-    version "4.8.28"
+    version "4.8.31"
 
     depends_on "bash"
     depends_on "jq"
 
     def install
         bin.install "maiass.sh" => "maiass"
-        bin.install "aicommit.sh" => "aicommit"
+        bin.install "committhis.sh" => "committhis"
         bin.install "package.json"
         bin.install_symlink "maiass" => "myass"
         bin.install_symlink "maiass" => "miass"
