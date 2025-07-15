2d900164bf8f28a1d0a64298ba9bd2e69c589ee9
vsmsh <vsmash@users.noreply.github.com>
2025-07-15 14:59:45 +1000

    Update to version 4.9.0

--- Formula/myass.rb
+++ Formula/myass.rb
@@ -1,10 +1,10 @@
 class Myass < Formula
     desc "Alias for MAIASS"
     homepage "https://github.com/vsmash/maiass"
-    url "https://github.com/vsmash/maiass/archive/refs/tags/4.8.31.tar.gz"
-    sha256 "0d10ad9c010f5d07663fabc00ec2b6d8200b1226ac00f8fc9cb1065503037800"
+    url "https://github.com/vsmash/maiass/archive/refs/tags/4.9.0.tar.gz"
+    sha256 "ad2167646e434492bd21cbc57ee96f0fe5086da6e8160506f1eb013cbf0a3b5b"
     license "GPL-3.0-only"
-    version "4.8.31"
+    version "4.9.0"
 
     depends_on "bash"
     depends_on "jq"
