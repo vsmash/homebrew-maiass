505fcc0a35edea21ca8a9556d26e4d351a9fcdaa
vsmsh <vsmash@users.noreply.github.com>
2025-08-02 13:39:14 +1000

    Update maiass to v5.3.6 with code-signed binaries

--- Formula/maiass.rb
+++ Formula/maiass.rb
@@ -2,22 +2,22 @@ class Maiass < Formula
   desc "MAIASS: Modular AI-Augmented Semantic Scribe - CLI tool for AI-augmented development"
   homepage "https://github.com/vsmash/maiass"
   url "https://github.com/vsmash/maiass/archive/refs/tags/#{version}.tar.gz"
-  version "5.3.4"
+  version "5.3.6"
 
   license "GPL-3.0-only"
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/vsmash/maiass/releases/download/#{version}/maiass-macos-x64.zip"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "20595b682b1804f335100adfa95e7b442785ee9d7a61f403baadf08964197916"
     else
       url "https://github.com/vsmash/maiass/releases/download/#{version}/maiass-macos-arm64.zip"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "901feb3554dafeb4a5381ee5f067bcd612c1ffbb655fd1415c5e0152397e2c64"
     end
   end
 
   on_linux do
     url "https://github.com/vsmash/maiass/releases/download/#{version}/maiass-linux-x64.tar.gz"
-    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    sha256 "882eb096f0b1c6a97ff7d6b9b9e1c91a54e0577f695ce6740d4fa2b0dd4e89d3"
   end
 
   def install
