40aab9405f83c319bd95ad2b6215cac9d37bb1ce
vsmsh <vsmash@users.noreply.github.com>
2025-08-04 07:46:17 +1000

    foo

--- Formula/maiass.rb
+++ Formula/maiass.rb
@@ -14,7 +14,6 @@ class Maiass < Formula
         bin.install "committhis.sh" => "committhis"
         bin.install "package.json"
         bin.install "lib"
-        bin.install "maiass.sh" => "maiass"
         bin.install_symlink "maiass" => "myass"
         bin.install_symlink "maiass" => "miass"
     end
