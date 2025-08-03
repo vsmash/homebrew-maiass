b93db55d028e652d27a077dd23edc3510918121f
vsmsh <vsmash@users.noreply.github.com>
2025-08-04 06:34:05 +1000

    Update to version 5.5.3

--- Formula/miass.rb
+++ Formula/miass.rb
@@ -1,10 +1,10 @@
 class Miass < Formula
     desc "Alias for MAIASS"
     homepage "https://github.com/vsmash/maiass"
-    url "https://github.com/vsmash/maiass/archive/refs/tags/5.5.2.tar.gz"
-    sha256 "75fd5ea79ab9c302f7e5f9d4e213148b707183f19af0790734fff3e79f2b8ac5"
+    url "https://github.com/vsmash/maiass/archive/refs/tags/5.5.3.tar.gz"
+    sha256 "4dda7800cd57d0a87e8053f77b51bb609113bf425920cc7c0f60a7a4c592f768"
     license "GPL-3.0-only"
-    version "5.5.2"
+    version "5.5.3"
 
     depends_on "bash"
     depends_on "jq"
