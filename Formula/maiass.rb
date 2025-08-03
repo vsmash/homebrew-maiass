a09f41d791340a05a17a29835408ebb832b8998d
vsmsh <vsmash@users.noreply.github.com>
2025-08-04 08:33:16 +1000

    update to formula

--- Formula/maiass.rb
+++ Formula/maiass.rb
@@ -15,16 +15,17 @@ class Maiass < Formula
         bin.install "package.json"
         bin.install_symlink "maiass" => "myass"
         bin.install_symlink "maiass" => "miass"
+        # Create a wrapper script that sets up the LIBEXEC_DIR
+        (bin/"maiass").write <<~EOS
+          #!/bin/bash
+          export LIBEXEC_DIR="#{libexec}/lib"
+          exec "#{opt_bin}/maiass" "$@"
+        EOS
+        chmod 0755, bin/"maiass"
     end
-    # Create a wrapper script that sets up the LIBEXEC_DIR
-    (bin/"maiass").write <<~EOS
-      #!/bin/bash
-      export LIBEXEC_DIR="#{libexec}/lib"
-      exec "#{opt_bin}/maiass" "$@"
-    EOS
+
 
     # Make the wrapper executable
-    chmod 0755, bin/"maiass"
     test do
       assert_match "MAIASS", shell_output("#{bin}/maiass --help")
     end
