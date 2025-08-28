496b079f8cf912df8c3a303431c25cb1005faa72
vsmsh <vsmash@users.noreply.github.com>
2025-08-29 07:16:06 +1000

    Update maiass to v5.9.6 (GitHub release)

--- Formula/maiass.rb
+++ Formula/maiass.rb
@@ -1,10 +1,10 @@
 class Maiass < Formula
   desc "Modular AI-Augmented Semantic Scribe for Git workflows"
   homepage "https://maiass.net"
-  url "https://github.com/vsmash/maiass/releases/download/v5.9.5/maiass-5.9.5.tar.gz"
-  sha256 "05853d6f83f31d4c7f55828ac7d3b6e853a49c1705a3cb6ced4c20201d208237"
+  url "https://github.com/vsmash/maiass/releases/download/v5.9.6/maiass-5.9.6.tar.gz"
+  sha256 "10dc22bae0c8d276923bafc8e0b8f826af0b8ddcde9f66b2a20491b49a5e53cc"
   license "GPL-3.0-only"
-  version "5.9.5"
+  version "5.9.6"
 
   depends_on "bash"
   depends_on "jq"
